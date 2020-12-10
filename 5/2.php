4ed7173a431d5d4a74f9c48b3d6e285b00b79038
Karol Wnuk <k.wnuk@ascetic.pl>
2020-12-10 21:40:42 +0100

    Day 5 part 2

---
+++ 5/2.php
@@ -0,0 +1,21 @@
+<?php
+
+$passes = explode(PHP_EOL, file_get_contents(__DIR__ . '/boarding.txt'));
+
+$halfOf = static fn(int $hi, int $lo): int => ceil(($hi - $lo) / 2);
+
+$reducer = static fn(array $acc, string $char): array => $char === 'F' || $char === 'L'
+    ? [$acc[0], $acc[1] - $halfOf($acc[1], $acc[0])]
+    : [$acc[0] + $halfOf($acc[1], $acc[0]), $acc[1]];
+
+$decoder = static fn(string $pass): int => 8
+    * array_reduce(str_split(substr($pass, 0, 7)), $reducer, [0, 127])[0]
+    + array_reduce(str_split(substr($pass, 7, 3)), $reducer, [0, 7])[0];
+
+$seats = array_map($decoder, $passes);
+
+sort($seats);
+
+for ($i = min($seats); in_array($i, $seats, true) && $i <= max($seats); $i++);
+
+echo $i;
